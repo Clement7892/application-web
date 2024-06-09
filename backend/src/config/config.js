89062b34210b6c86756d53dbbfd1437a3fd2ae7c
Clement7892 <clement.roc2002@gmail.com>
2024-06-09 22:46:40 +0200

    render

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,5 +1,7 @@
 const config = {
-  mongoURI: process.env.MONGO_URI || "your-default-mongo-uri",
+  mongoURI:
+    process.env.MONGO_URI ||
+    "mongodb+srv://dbUser:gWNO7JV9Nck02TA2@application-web.vt8o0fe.mongodb.net/application-web?retryWrites=true&w=majority",
 };
 
 module.exports = config;
