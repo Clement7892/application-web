9e0620e16c4b83d8f3d6718a8ad98913996895c4
Clement7892 <clement.roc2002@gmail.com>
2024-06-09 21:21:28 +0200

    render

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@application-web.vt8o0fe.mongodb.net/?retryWrites=true&w=majority`,
-  secretOrKey: "secret",
+const config = {
+  mongoURI: process.env.MONGO_URI || "your-default-mongo-uri",
 };
+
+module.exports = config;
