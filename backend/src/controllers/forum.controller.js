148f08b1b903c4b8429465b337e707e4f2717f8f
Clement7892 <clement.roc2002@gmail.com>
2024-06-10 18:28:46 +0200

    render

--- backend/src/controllers/forum.controller.js
+++ backend/src/controllers/forum.controller.js
@@ -41,33 +41,22 @@ exports.updateMessage = async (req, res) => {
   try {
     const { id } = req.params;
     const { content } = req.body;
-    const { userId } = req.user; // Obtenez l'ID de l'utilisateur à partir du JWT
-
-    // Recherchez le message dans la base de données par son ID
+    const { userId } = req.user;
     const message = await Message.findById(id);
-
-    // Vérifiez si le message existe
     if (!message) {
       return res
         .status(404)
         .json({ success: false, message: "Message not found" });
     }
 
-    // Vérifiez si l'utilisateur actuel est l'auteur du message
     if (message.userId.toString() !== userId) {
-      // Si l'ID de l'utilisateur du message ne correspond pas à l'ID de l'utilisateur actuel, renvoyez une erreur d'authentification
       return res.status(403).json({ success: false, message: "Unauthorized" });
     }
 
-    // Mettez à jour le contenu du message
     message.content = content;
-    // Enregistrez les modifications dans la base de données
     await message.save();
-
-    // Répondez avec succès et renvoyez le message mis à jour
     res.status(200).json({ success: true, message });
   } catch (error) {
-    // Gérez les erreurs et renvoyez un message d'erreur approprié
     console.error("Error updating message:", error);
     res.status(500).json({ success: false, error: error.message });
   }
